422236abda3b915a309560042e9c5eeb53a250ae
sachinpink <sachinzagade59@gmail.com>
2025-02-16 12:07:21 +0530

    commit 1

--- src/test/java/testRunners/ParallelRunner.java
+++ src/test/java/testRunners/ParallelRunner.java
@@ -3,6 +3,8 @@ package testRunners;
 import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+
+//CucumberOptions  used to link feature file & stepDefinitions
 @CucumberOptions(
 
 		features= {"src/test/resources/features/"},
