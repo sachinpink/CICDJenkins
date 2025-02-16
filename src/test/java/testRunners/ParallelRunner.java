7069777e6fa6d7118e7a60f8562052886da12b41
sachinpink <sachinzagade59@gmail.com>
2025-02-16 11:54:34 +0530

    use of Cucuber option

--- src/test/java/testRunners/ParallelRunner.java
+++ src/test/java/testRunners/ParallelRunner.java
@@ -4,7 +4,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-
+//CucumberOptions  used to link feature file & stepDefinitions
 @CucumberOptions(
 
 		features= {"src/test/resources/features/"},
